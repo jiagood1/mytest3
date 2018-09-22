daee780925c51252823719888bbee490817c9ec9
路人甲 <1105712216@qq.com>
2018-09-22 21:50:37 +0800

    添加test工程到git

---
+++ day01-git/src/com/itcast/Test.java
@@ -0,0 +1,10 @@
+package com.itcast;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("圣诞节卡来访多了");
+        System.out.println("圣诞节卡来访多了");
+        System.out.println("圣诞节卡来访多了");
+        System.out.println("圣诞节卡来访多了");
+    }
+}
